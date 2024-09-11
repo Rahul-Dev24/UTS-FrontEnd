10666cbde1747fbebb369d3612e387a7ba78ead2
Rahul-Dev24 <149306097+Rahul-Dev24@users.noreply.github.com>
2024-09-11 19:49:58 +0530

    Session Section Added

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Layout/Home/Home";
+import Splash from "./Components/splashScreen/Splash";
+import "../src/assets/style.css";
+import StationSearch from "./Components/stationSearch/StationSearch";
+import GetFare from "./Components/getFare/GetFare";
+import Login from "./Components/login/Login";
+import SessionTicketIssue from "./Layout/SessionTicket/SessionTicketIssue";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Splash />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/home/:url" element={<Home />} />
+          <Route path="/stationSearch/:url" element={<StationSearch />} />
+          <Route path="/getFare" element={<GetFare />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/getSessionTicket" element={<SessionTicketIssue />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
