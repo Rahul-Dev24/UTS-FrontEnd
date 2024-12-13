a652e83f1c4f570c9d924fdbe11b55b892f75c4f
Rahul-Dev24 <149306097+Rahul-Dev24@users.noreply.github.com>
2024-12-13 19:50:14 +0530

    Delete src directory

--- src/utils/Decode.js
+++
@@ -1,9 +0,0 @@
-import CryptoJS from "crypto-js";
-
-export const Decryption = (url) => {
-    const secretKey = "fsdfgoiuewnskj812jndsf"; // Use a secure key
-    // Decrypt the string
-    const bytes = CryptoJS.AES.decrypt(decodeURIComponent(url), secretKey);
-    const decryptedObject = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-    return decryptedObject;
-}
